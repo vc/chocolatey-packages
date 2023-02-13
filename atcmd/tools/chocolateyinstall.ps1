28b435b9a1dfab8e57939febecf9d7bd24d1c77a
strelok.vc <strelok.vc@gmail.com>
2023-02-13 15:49:27 +0300

    Initiate commit

---
+++ atcmd/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$installDir = $toolsDir
+
+$packageArgs = @{
+  packageName    = 'atcmd'
+  url            = 'https://cdn.atraining.ru/repo/ATcmd/atcmd-313.zip'
+  checksum       = '778ea01582051ed92472a8ce5d35cf01b9aceead473abf90f99f0ed56bcf22f5'
+  checksumType   = 'sha256'
+  unzipLocation  = $installDir
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ svcadmin/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#$pp = Get-PackageParameters
+$installDir = $toolsDir
+#if ($pp.InstallDir -or $pp.InstallationPath ) { $InstallDir = $pp.InstallDir + $pp.InstallationPath }
+#Write-Host "Sysinternals Suite is going to be installed in '$installDir'"
+
+$packageArgs = @{
+  packageName    = 'svcadmin'
+  url            = 'https://rsdn.org/article/baseserv/svcadmin-1/svcadmin.zip'
+  checksum       = 'f04ee7bc0179b688d4cf31be9ec23a14fa4a5bb60b0a0a7b10f0c2c4b9e0165c'
+  checksumType   = 'sha256'
+  unzipLocation  = $installDir
+}
+Install-ChocolateyZipPackage @packageArgs
