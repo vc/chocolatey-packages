ca293a02907d4b682f0800c6fbfea2bb604fd635
strelok.vc <strelok.vc@gmail.com>
2026-05-15 03:20:09 +0300

    atcmd: update install to use built-in helper, remove curl dependency

--- atcmd/tools/chocolateyinstall.ps1
+++ atcmd/tools/chocolateyinstall.ps1
@@ -1,24 +1,14 @@
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $installDir = $toolsDir
 
-$chocoBin = "$env:ChocolateyInstall\bin"
-$env:PATH = "$chocoBin;$env:PATH"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$url = 'https://cdn.atraining.ru/repo/ATcmd/atcmd-313.zip'
-$checksum = '778ea01582051ed92472a8ce5d35cf01b9aceead473abf90f99f0ed56bcf22f5'
-$zipPath = "$env:TEMP\atcmd_temp.zip"
-
-& "$chocoBin\curl.exe" -sL -o $zipPath $url
-if ($LASTEXITCODE -ne 0) {
-    throw "Download failed with exit code $LASTEXITCODE"
+$packageArgs = @{
+  packageName    = 'atcmd'
+  url            = 'https://cdn.atraining.ru/repo/ATcmd/atcmd-313.zip'
+  checksum       = '778ea01582051ed92472a8ce5d35cf01b9aceead473abf90f99f0ed56bcf22f5'
+  checksumType   = 'sha256'
+  unzipLocation  = $installDir
 }
-
-$hash = Get-FileHash -Path $zipPath -Algorithm SHA256
-if ($hash.Hash -ne $checksum) {
-    Remove-Item $zipPath -Force
-    throw "Checksum mismatch. Expected: $checksum, Actual: $($hash.Hash)"
-}
-
-Expand-Archive -Path $zipPath -DestinationPath $installDir -Force
-Remove-Item $zipPath -Force
+Install-ChocolateyZipPackage @packageArgs
